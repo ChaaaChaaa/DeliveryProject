ec12d9eb17d35c72ebfd52a7a57cb99cea7ea6ed
peony <peony@bluehole.net>
2023-03-19 10:08:32 +0900

    feat createFood관련 메소드 FoodService 인터페이스에 추가

--- src/main/java/com/programmers/service/FoodService.java
+++ src/main/java/com/programmers/service/FoodService.java
@@ -1,26 +1,6 @@
 package com.programmers.service;
 
 import com.programmers.domain.Food;
-import com.programmers.request.FoodUpdateRequest;
-import com.programmers.response.FoodResponse;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FoodService {
-    public void saveFood(Food food){
-
-    }
-
-    public List<FoodResponse> searchAllFoods(){
-        return new ArrayList<>();
-    }
-
-    public void deleteFoodId(Long foodId){
-
-    }
-
-    public void updateFood(Long id, FoodUpdateRequest foodUpdateRequest){
-
-    }
+public interface FoodService {
+    void create(Food food);
 }
