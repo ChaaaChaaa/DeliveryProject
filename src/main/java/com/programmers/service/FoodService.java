7c6c6dfe64e9f72d7d2615e9c661d2752201c170
peony <peony@bluehole.net>
2023-03-19 10:15:32 +0900

    feat search foodId 메소드 FoodService 추가

--- src/main/java/com/programmers/service/FoodService.java
+++ src/main/java/com/programmers/service/FoodService.java
@@ -1,9 +1,15 @@
 package com.programmers.service;
 
 import com.programmers.domain.Food;
+import com.programmers.dto.FoodRequestDto;
+import com.programmers.dto.FoodResponseDto;
+import com.programmers.request.FoodUpdateRequest;
 
+import java.util.List;
+import java.util.Optional;
 
 public interface FoodService {
     void create(Food food);
     Food save(Food food);
+    Optional<Food> findById(Long id);
 }
