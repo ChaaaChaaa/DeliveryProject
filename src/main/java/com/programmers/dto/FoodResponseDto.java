192f2ae85ac752ebfff8df16ef92561ef56127e6
peony <peony@bluehole.net>
2023-03-19 10:04:03 +0900

    feat 데이터 전달을 위한 FoodResponseDto 생성

---
+++ src/main/java/com/programmers/dto/FoodResponseDto.java
@@ -0,0 +1,27 @@
+package com.programmers.dto;
+
+import com.programmers.domain.Food;
+import com.programmers.request.FoodUpdateRequest;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FoodResponseDto {
+    private String name;
+    private String category;
+    private int price;
+    private String description;
+    private String image;
+
+    public FoodResponseDto(Food food) {
+        this.name = food.getName();
+        this.category = food.getCategory();
+        this.price = food.getPrice();
+        this.description = food.getDescription();
+        this.image = food.getImage();
+    }
+}
