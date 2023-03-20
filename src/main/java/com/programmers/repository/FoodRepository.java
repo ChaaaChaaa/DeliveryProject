ef542ac003f64b7f66da4697f3108b8ef5c838db
peony <peony@bluehole.net>
2023-03-20 22:45:12 +0900

    refactor createFood의 위치 이동

--- src/main/java/com/programmers/repository/FoodRepository.java
+++ src/main/java/com/programmers/repository/FoodRepository.java
@@ -3,23 +3,17 @@ package com.programmers.repository;
 import com.programmers.domain.Food;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+import lombok.NonNull;
+
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
-    default void createFood(Food food) {
-        Food.builder()
-                .category(food.getCategory())
-                .name(food.getName())
-                .price(food.getPrice())
-                .description(food.getDescription())
-                .image(food.getImage())
-                .build();
-    }
-
+    @NonNull
     Optional<Food> findById(Long id);
 
     List<Food> findByNameContaining(String Name);
