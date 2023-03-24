f67667a8e03f73196100c3efa52533b9c3e72c9f
peony <peony@bluehole.net>
2023-03-24 17:58:11 +0900

    refactor builder 삭제

--- src/main/java/com/programmers/domain/Food.java
+++ src/main/java/com/programmers/domain/Food.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,17 +26,5 @@ public class Food {
 
     @Column(columnDefinition = "TEXT")
     private String description;
-
     private String image;
-
-
-    @Builder
-    public Food(Long id, String category, String name, int price, String description, String image) {
-        this.id = id;
-        this.category = category;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.image = image;
-    }
 }
