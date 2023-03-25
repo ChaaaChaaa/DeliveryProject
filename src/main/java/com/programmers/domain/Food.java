a8dee055476519a2f4b7727bb126cd0b91002ae4
peony <peony@bluehole.net>
2023-03-26 04:11:09 +0900

    bugfix id 자동생성으로 변경

--- src/main/java/com/programmers/domain/Food.java
+++ src/main/java/com/programmers/domain/Food.java
@@ -2,6 +2,8 @@ package com.programmers.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -13,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Food {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(length = 30, nullable = false)
