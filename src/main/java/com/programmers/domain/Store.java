10338f18618810067315f7ffed88f67817ffe49b
peony <peony@bluehole.net>
2023-03-26 04:19:12 +0900

    feat store domain 추가

---
+++ src/main/java/com/programmers/domain/Store.java
@@ -0,0 +1,46 @@
+package com.programmers.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storeId;
+
+    @Column(length = 30, nullable = false)
+    private String storeName;
+
+    @Column(length = 20, nullable = false)
+    private String category;
+
+    @Column(length = 11, nullable = false)
+    private int reviewCount;
+    private float rating;
+
+    @Builder
+    public Store(Long storeId, String storeName, String category, int reviewCount, float rating) {
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.category = category;
+        this.reviewCount = reviewCount;
+        this.rating = rating;
+    }
+
+    public void update(String storeName, String category, int reviewCount, float rating) {
+        this.storeName = storeName;
+        this.category = category;
+        this.reviewCount = reviewCount;
+        this.rating = rating;
+    }
+}
