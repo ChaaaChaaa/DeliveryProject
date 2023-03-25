04e3dfa5c3a4bd6bcc5dbe4cd842174f7292028c
peony <peony@bluehole.net>
2023-03-26 04:19:33 +0900

    feat StoreController 추가

---
+++ src/main/java/com/programmers/controller/StoreController.java
@@ -0,0 +1,53 @@
+package com.programmers.controller;
+
+import com.programmers.domain.Store;
+import com.programmers.dto.store.StoreResponseDto;
+import com.programmers.dto.store.StoreUpdateRequestDto;
+import com.programmers.service.StoreService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+@RestController
+public class StoreController {
+    private final StoreService storeService;
+
+    @PostMapping("/save")
+    public void saveStore(@RequestBody Store store) {
+        storeService.save(store);
+    }
+
+
+    @GetMapping("/{storeId}")
+    public StoreResponseDto searchStoreById(@PathVariable Long storeId) {
+        return storeService.findByStoreId(storeId);
+    }
+
+    @GetMapping("/search")
+    public StoreResponseDto searchStoreByName(@RequestParam("name") String storeName) {
+        return storeService.findByStoreName(storeName);
+    }
+
+    @PutMapping("/{storeId}")
+    public void updateStore(@PathVariable Long storeId, @RequestBody StoreUpdateRequestDto storeUpdateRequestDto) {
+        storeService.update(storeId, storeUpdateRequestDto);
+    }
+
+    @DeleteMapping("/{storeId}")
+    public void deleteStoreId(@PathVariable Long storeId) {
+        storeService.deleteById(storeId);
+    }
+
+}
