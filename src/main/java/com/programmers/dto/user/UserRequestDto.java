128cf9432858b81ba154526b83e7b8d81ddde427
최미정 <peony@krafton.com>
2023-03-28 13:19:16 +0900

    feat user request dto 추가

---
+++ src/main/java/com/programmers/dto/user/UserRequestDto.java
@@ -0,0 +1,43 @@
+package com.programmers.dto.user;
+
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Builder;
+
+public class UserRequestDto {
+    @NotNull
+    private String userId;
+    @NotBlank
+    @Length(min = 1, max = 30)
+    private String name;
+    @NotBlank
+    @Length(min = 1, max = 30)
+    private String password;
+    @NotBlank
+    @Length(min = 1, max = 30)
+    private String nickName;
+    private String grade;
+    @NotBlank
+    @Length(min = 1, max = 30)
+    private int phoneNumber;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public UserRequestDto(String userId, String name, String password, String nickName, int phoneNumber) {
+        this.userId = userId;
+        this.name = name;
+        this.password = password;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+    }
+}
