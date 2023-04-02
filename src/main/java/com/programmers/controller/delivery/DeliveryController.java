5104058fa5b6dca893d73c1abbb2c0f1fdf21cc0
chacha <peony@bluehole.net>
2023-04-02 11:12:14 +0900

    feat DeliveryController에 save 기능 추가

---
+++ src/main/java/com/programmers/controller/delivery/DeliveryController.java
@@ -0,0 +1,19 @@
+package com.programmers.controller.delivery;
+
+import com.programmers.dto.delivery.DeliveryResponseDto;
+import com.programmers.service.delivery.DeliveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/delivery")
+@RequiredArgsConstructor
+public class DeliveryController {
+    private final DeliveryService deliveryService;
+
+    @PostMapping("/save")
+    public void saveDelivery(@RequestBody DeliveryResponseDto deliveryResponseDto){
+        deliveryService.save(deliveryResponseDto);
+    }
+
+}
