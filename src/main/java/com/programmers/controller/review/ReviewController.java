362662cab38503864b66b1478ab10a03938cc0ec
chacha <peony@bluehole.net>
2023-04-02 11:18:26 +0900

    feat ReviewController에 save 기능 추가

---
+++ src/main/java/com/programmers/controller/review/ReviewController.java
@@ -0,0 +1,25 @@
+package com.programmers.controller.review;
+
+import com.programmers.domain.review.Review;
+import com.programmers.domain.store.Store;
+import com.programmers.dto.review.ReviewRequestDto;
+import com.programmers.dto.review.ReviewResponseDto;
+import com.programmers.service.review.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/review")
+@RestController
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @PostMapping(path = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveReview(@RequestBody ReviewRequestDto reviewRequestDto) {
+        reviewService.save(reviewRequestDto);
+    }
+
+}
