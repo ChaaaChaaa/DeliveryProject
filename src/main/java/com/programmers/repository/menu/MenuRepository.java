27632b3cf56e9500ca24a9cc92cd08bff668368a
chacha <peony@bluehole.net>
2023-04-02 11:55:49 +0900

    feat Menu,Store 찾기 기능 추가

--- src/main/java/com/programmers/repository/menu/MenuRepository.java
+++ src/main/java/com/programmers/repository/menu/MenuRepository.java
@@ -16,6 +16,10 @@ public interface MenuRepository extends JpaRepository<Menu,Long> {
    @NotNull
     Optional<Menu> findById(@Nullable Long menuId);
 
+    Optional<Menu> findByMenu(Long menuId);
+
+    Store findStoreByMenuId(Long menuId);
+
     Menu save(@Nullable Menu menu);
 
     void delete(@Nullable Menu menu);
