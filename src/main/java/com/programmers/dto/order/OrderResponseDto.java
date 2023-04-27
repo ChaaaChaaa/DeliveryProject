4f783fa0968e480a23f11cc1e0ca0415b62e524f
chacha <peony@bluehole.net>
2023-04-28 01:29:44 +0900

    refactor table 변경으로 menu관련된 클래스 모두 삭제

--- src/main/java/com/programmers/dto/order/OrderResponseDto.java
+++ src/main/java/com/programmers/dto/order/OrderResponseDto.java
@@ -1,42 +1,50 @@
 package com.programmers.dto.order;
 
 import com.programmers.domain.delivery.Delivery;
-import com.programmers.domain.menu.Menu;
-import com.programmers.domain.order.Order;
+import com.programmers.domain.order.OrderList;
+import com.programmers.domain.order.OrderState;
+import com.programmers.domain.order.Payment;
+import com.programmers.domain.orderItem.OrderItem;
 import com.programmers.domain.user.User;
 
+import java.util.List;
+
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 public class OrderResponseDto {
     private final Long orderId;
     private final User user;
-    private final Menu menu;
     private final Delivery delivery;
-    private final String paymentMethod;
-    private final String state;
-    private final int price;
+    private final Payment paymentMethod;
+    private final OrderState orderState;
+    private final int totalPrice;
+    private final List<OrderItem> orderItems;
 
     @Builder
-    public OrderResponseDto(Long orderId, User user, Menu menu, Delivery delivery, String paymentMethod, String state, int price) {
+    public OrderResponseDto(Long orderId, User user, Delivery delivery, Payment paymentMethod, OrderState orderState, int totalPrice, List<OrderItem> orderItems) {
         this.orderId = orderId;
         this.user = user;
-        this.menu = menu;
         this.delivery = delivery;
         this.paymentMethod = paymentMethod;
-        this.state = state;
-        this.price = price;
+        this.orderState = orderState;
+        this.totalPrice = totalPrice;
+        this.orderItems = orderItems;
     }
 
-    public static OrderResponseDto of(Order order) {
+
+    public static OrderResponseDto of(OrderList orderList) {
         return OrderResponseDto.builder()
-                .orderId(order.getOrderId())
-                .user(order.getUser())
-                .menu(order.getMenu())
-                .delivery(order.getDelivery())
-                .paymentMethod(order.getPaymentMethod())
-                .state(order.getState())
-                .price(order.getPrice())
+                .orderId(orderList.getOrderListId())
+                .user(orderList.getUser())
+                .delivery(orderList.getDelivery())
+                .paymentMethod(orderList.getPaymentMethod())
+                .orderState(orderList.getOrderState())
+                .totalPrice(orderList.getTotalPrice())
+                .orderItems(orderList.getOrderItems())
                 .build();
     }
 }
