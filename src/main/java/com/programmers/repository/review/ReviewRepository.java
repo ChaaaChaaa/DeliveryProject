60b1871b536e276531771d942b906d315a8bb88d
chacha <peony@bluehole.net>
2023-04-28 09:37:09 +0900

    feat Repository Annotation 추가

--- src/main/java/com/programmers/repository/review/ReviewRepository.java
+++ src/main/java/com/programmers/repository/review/ReviewRepository.java
@@ -3,9 +3,11 @@ package com.programmers.repository.review;
 import com.programmers.domain.review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Optional<Review> findById(@Nullable Long reviewId);
     Optional<Review> findByStore(@Nullable Long reviewId); //메뉴 테이블에 있는데 findByStore하는게 맞는가?
--- src/main/java/com/programmers/repository/user/UserRepository.java
+++ src/main/java/com/programmers/repository/user/UserRepository.java
@@ -4,9 +4,11 @@ import com.programmers.domain.user.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findById(Long userId);
