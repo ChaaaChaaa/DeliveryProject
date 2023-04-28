33c7528d45a3bc6c8e259657f61ad181770018cc
chacha <peony@bluehole.net>
2023-04-28 09:40:16 +0900

    feat OrderListService 추가

---
+++ src/main/java/com/programmers/service/orderList/OrderListService.java
@@ -0,0 +1,69 @@
+package com.programmers.service.orderList;
+
+import com.programmers.domain.delivery.Delivery;
+import com.programmers.domain.order.OrderList;
+import com.programmers.domain.user.User;
+import com.programmers.dto.order.OrderRequestDto;
+import com.programmers.dto.order.OrderResponseDto;
+import com.programmers.exception.delivery.DeliveryNotFoundException;
+import com.programmers.exception.user.UserNotFoundException;
+import com.programmers.repository.delivery.DeliveryRepository;
+import com.programmers.repository.order.OrderListRepository;
+import com.programmers.repository.orderItem.OrderItemRepository;
+import com.programmers.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.transaction.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class OrderListService {
+    private final OrderListRepository orderListRepository;
+    private final UserRepository userRepository;
+    private final DeliveryRepository deliveryRepository;
+    private final OrderItemRepository orderItemRepository;
+
+@Transactional
+    public OrderList save(OrderRequestDto orderRequestDto) {
+        // order save
+        User user = userRepository.findByUserId(orderRequestDto.getUser().getUserId()).orElseThrow(UserNotFoundException::new);
+        Delivery delivery = deliveryRepository.findById(orderRequestDto.getDelivery().getDeliveryId()).orElseThrow(DeliveryNotFoundException::new);
+
+        OrderList savedOrderList = OrderList.builder()
+                .user(user)
+                .delivery(delivery)
+                .paymentMethod(orderRequestDto.getPaymentMethod())
+                .orderState(orderRequestDto.getOrderState())
+                .totalPrice(orderRequestDto.getTotalPrice())
+                .build();
+        orderListRepository.save(savedOrderList);
+
+        // orderItemList save
+        orderItemRepository.saveAll(orderRequestDto.getOrderItems());
+        return savedOrderList;
+    }
+
+    public OrderResponseDto findById(Long orderListId) {
+        return OrderResponseDto.of(orderListRepository.findById(orderListId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 id의 주문이 존재하지 않습니다.")));
+    }
+
+    public User findUserByOrderListId(Long orderListId){
+        OrderList orderList = orderListRepository.findById(orderListId).orElseThrow(UserNotFoundException::new);
+        return userRepository.findByUserId(orderList.getUser().getUserId()).orElseThrow(UserNotFoundException::new);
+    }
+
+
+    public Delivery findDeliveryByOrderListId(Long orderListId){
+        OrderList orderList = orderListRepository.findById(orderListId).orElseThrow(UserNotFoundException::new);
+        return deliveryRepository.findById(orderList.getDelivery().getDeliveryId()).orElseThrow(DeliveryNotFoundException::new);
+    }
+
+    public void deleteById(long orderListId){
+        orderListRepository.deleteById(orderListId);
+    }
+}
