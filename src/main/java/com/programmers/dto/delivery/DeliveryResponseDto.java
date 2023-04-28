b82bc96cdc05f97e64863ffcf2d6e7b65b47bd27
chacha <peony@bluehole.net>
2023-04-28 09:19:57 +0900

    feat Setter Annotation 추가 및 Delivery domain을 DeliveryResponseDto로 변환시키는 of메소드 추가

--- src/main/java/com/programmers/dto/delivery/DeliveryResponseDto.java
+++ src/main/java/com/programmers/dto/delivery/DeliveryResponseDto.java
@@ -1,12 +1,16 @@
 package com.programmers.dto.delivery;
 
 import com.programmers.domain.delivery.Delivery;
+
+import javax.validation.constraints.NotNull;
+
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
+@Setter
 @EqualsAndHashCode
 public class DeliveryResponseDto {
     @NotNull
@@ -15,5 +19,11 @@ public class DeliveryResponseDto {
     @Builder
     public DeliveryResponseDto(long deliveryId) {
         this.deliveryId = deliveryId;
+    }
+
+    public static DeliveryResponseDto of(Delivery delivery){
+        return DeliveryResponseDto.builder()
+                .deliveryId(delivery.getDeliveryId())
+                .build();
     }
 }
