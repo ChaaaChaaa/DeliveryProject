13daf1fc4f458eb5c8f2116925bd99c41ece5711
chacha <peony@bluehole.net>
2023-04-28 09:51:48 +0900

    refactor findByFoodId를 findById로 메소드명 변경

--- src/main/java/com/programmers/controller/food/FoodController.java
+++ src/main/java/com/programmers/controller/food/FoodController.java
@@ -32,7 +32,7 @@ public class FoodController {
 
     @GetMapping("/{id}")
     public FoodResponseDto searchFoodById(@PathVariable Long id) {
-        return foodService.findByFoodId(id);
+        return foodService.findById(id);
     }
 
 
