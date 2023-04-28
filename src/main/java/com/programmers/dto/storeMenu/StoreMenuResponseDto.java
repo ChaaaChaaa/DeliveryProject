9a3dec77c4438a223e73c287204431c7ea595b2c
chacha <peony@bluehole.net>
2023-04-28 09:26:47 +0900

    feat storeMenu Request/Response class 추가

---
+++ src/main/java/com/programmers/dto/storeMenu/StoreMenuResponseDto.java
@@ -0,0 +1,30 @@
+package com.programmers.dto.storeMenu;
+
+import com.programmers.domain.storeMenu.StoreMenu;
+
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class StoreMenuResponseDto {
+    private String storeName;
+    private String foodName;
+
+    @Builder
+    public StoreMenuResponseDto(String storeName, String foodName) {
+        this.storeName = storeName;
+        this.foodName = foodName;
+    }
+
+    public static StoreMenuResponseDto convertIdToName(StoreMenu storeMenu) {
+        return StoreMenuResponseDto.builder()
+                .foodName(storeMenu.getFood().getName())
+                .storeName(storeMenu.getStore().getStoreName())
+                .build();
+    }
+}
